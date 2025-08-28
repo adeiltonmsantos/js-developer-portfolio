96156ac10f14e6dee7fbd2239bf1d61cc8474116
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2025-08-28 14:35:36 -0300

    Ajusting profile data in profile.json

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,6 +2,5 @@
 async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
     const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+    return await response.json()
 }
